00eb78fa54a3ec04a508e261c8ef90195dec829f
Rohit Ranjan <rohitgns54321@gmail.com>
2023-08-08 15:48:31 +0530

    FIRST COMMIT

---
+++ lib/home_screen.dart
@@ -0,0 +1,136 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:http/http.dart' as http;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  Map<String,dynamic>? paymentIntentData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stripe Payment'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          InkWell(
+            onTap: ()async{
+              await makePayment();
+            },
+            child: Container(
+              height: 50,
+            width: 200,
+              decoration: BoxDecoration(
+                color: Colors.green
+              ),
+              child: Center(
+                child: Text('Pay'),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<void> makePayment() async {
+    try {
+      paymentIntentData =
+      await createPaymentIntent('20', 'USD'); //json.decode(response.body);
+      // print('Response body==>${response.body.toString()}');
+      await Stripe.instance
+          .initPaymentSheet(
+          paymentSheetParameters: SetupPaymentSheetParameters(
+              setupIntentClientSecret: 'Your Secret Key',
+              paymentIntentClientSecret:
+              paymentIntentData!['client_secret'],
+              //applePay: PaymentSheetApplePay.,
+              //googlePay: true,
+              //testEnv: true,
+              customFlow: true,
+              style: ThemeMode.dark,
+              // merchantCountryCode: 'US',
+              merchantDisplayName: 'TECHHIVE'))
+          .then((value) {});
+
+      ///now finally display payment sheeet
+      displayPaymentSheet();
+    } catch (e, s) {
+      print('Payment exception:$e$s');
+    }
+  }
+
+  displayPaymentSheet() async {
+    try {
+      await Stripe.instance
+          .presentPaymentSheet(
+        //       parameters: PresentPaymentSheetParameters(
+        // clientSecret: paymentIntentData!['client_secret'],
+        // confirmPayment: true,
+        // )
+      )
+          .then((newValue) {
+        print('payment intent' + paymentIntentData!['id'].toString());
+        print(
+            'payment intent' + paymentIntentData!['client_secret'].toString());
+        print('payment intent' + paymentIntentData!['amount'].toString());
+        print('payment intent' + paymentIntentData.toString());
+        //orderPlaceApi(paymentIntentData!['id'].toString());
+        ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text("paid successfully")));
+
+        paymentIntentData = null;
+      }).onError((error, stackTrace) {
+        print('Exception/DISPLAYPAYMENTSHEET==> $error $stackTrace');
+      });
+    } on StripeException catch (e) {
+      print('Exception/DISPLAYPAYMENTSHEET==> $e');
+      showDialog(
+          context: context,
+          builder: (_) => const AlertDialog(
+            content: Text("Cancelled "),
+          ));
+    } catch (e) {
+      print('$e');
+    }
+  }
+
+
+  createPaymentIntent(String amount,String currency) async {
+    try{
+      Map<String,dynamic> body = {
+        'amount': calculateAmount(amount),
+        'currency':currency,
+        'payment_method_types[]': 'card'
+      };
+
+      var response = await http.post(Uri.parse('https://api.stripe.com/v1/payment_intents'),
+      body: body,
+        headers: {
+        'Authorization': 'Bearer https://api.stripe.com/v1/payment_intents',
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      );
+      print('Create Intent reponse ===> ${response.body.toString()}');
+      return jsonDecode(response.body);
+    }catch(e){
+      print('Exception'+e.toString());
+    }
+  }
+  calculateAmount(String amount){
+    final price = int.parse(amount) * 100;
+    return price;
+  }
+
+}
